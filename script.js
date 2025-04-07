6a1731c1ea66865dd76fe67ffef5b04f64c4b5f3
An1by <anibycor@gmail.com>
2025-04-07 11:48:37 +0300

    fix: don't deleting old words now feat: new ru/en words

--- script.js
+++ script.js
@@ -1,25 +1,64 @@
-let filterNames = ["russian_ban_words", "english_bad_words", "symbols"]
-let words = [];
-
-for await (let filterName of filterNames) {
-    const response = await fetch(
-        `https://raw.githubusercontent.com/an1by/DonationAlertsFilter/refs/heads/master/filters/${filterName}.txt`
-    );
-    const responseWords = (await response.text()).replaceAll("\n", " ");
-    words.push(responseWords);
+async function getAlreadyBlacklistedWords() {
+    const tokenResponse = await fetch("https://www.donationalerts.com/api/v1/session/token?spa_page=dashboard", {
+        headers: {
+            "Accept": "application/json, text/plain, */*",
+            "Referer": "https://www.donationalerts.com/dashboard/donations-settings/blacklist",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        method: "GET",
+        credentials: "same-origin"
+    });
+    const apiToken = (await tokenResponse.json()).data.api_token
+    
+    const userResponse = await fetch("https://www.donationalerts.com/api/v1/user", {
+        headers: {
+            "Authorization": `Bearer ${apiToken}`,
+            "Accept": "application/json, text/plain, */*",
+            "Referer": "https://www.donationalerts.com/dashboard/donations-settings/blacklist",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        method: "GET",
+        credentials: "same-origin"
+    });
+    return (await userResponse.json()).data.black_list_words;
 }
 
-var encodedData = encodeURIComponent(`main_currency=RUB&timezone=Europe%2FMoscow&language=ru_RU&remove_links=1&black_list_words=${words.join(" ")}&words=&email_news=0`);
-var body = `data=${encodedData}&form=settings_general`
+async function getFilterWords() {
+    const filterNames = ["russian_ban_words", "english_bad_words", "symbols"]
+    const words = [];
 
-fetch("https://www.donationalerts.com/savesettings/general", {
-    "headers": {
-        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "x-requested-with": "XMLHttpRequest",
-        "Referer": "https://www.donationalerts.com/dashboard/donations-settings/blacklist",
-        "Referrer-Policy": "strict-origin-when-cross-origin"
-    },
-    "method": "POST",
-    "credentials": "same-origin",
-    "body": body
-});
+    for await (const filterName of filterNames) {
+        const response = await fetch(
+            `https://raw.githubusercontent.com/an1by/DonationAlertsFilter/refs/heads/master/filters/${filterName}.txt`
+        );
+        const responseWords = (await response.text()).replaceAll("\n", " ");
+        words.push(responseWords);
+    }
+
+    return words;
+}
+
+async function setBlacklistedWords(words) {
+    const encodedData = encodeURIComponent(`main_currency=RUB&timezone=Europe%2FMoscow&language=ru_RU&remove_links=1&black_list_words=${words.join(" ")}&words=&email_news=0`);
+    const body = `data=${encodedData}&form=settings_general`
+
+    await fetch("https://www.donationalerts.com/savesettings/general", {
+        headers: {
+            "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "x-requested-with": "XMLHttpRequest",
+            "Referer": "https://www.donationalerts.com/dashboard/donations-settings/blacklist",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        method: "POST",
+        credentials: "same-origin",
+        body: body
+    });
+
+    return "Все готово!";
+}
+
+const existWords = await getAlreadyBlacklistedWords();
+const filterWords = await getFilterWords();
+const words = [...new Set([...existWords, ...filterWords])];
+
+await setBlacklistedWords(words);
